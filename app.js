be371356b9e90ef2dc94de57dfcaca1c0e051dd9
MariaRipley <roblesb.maria@gmail.com>
2023-09-01 13:01:43 +0200

    Create function to show person

--- app.js
+++ app.js
@@ -47,6 +47,16 @@ let currentItem = 0;
 
 // Load initial item
 window.addEventListener('DOMContentLoaded', function() {
-    const item = reviews[currentItem];
-    img.src = item.img;
+    showPerson(currentItem);
 });
+
+// Show person based on item
+function showPerson(person) {
+    const item = reviews[person];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+};
+
+// Show next person
